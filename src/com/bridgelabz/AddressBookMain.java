73c172528020fe1bf52a0abaaa3f83037d7fe855
Ananthdev24796 <ananth.cr7@gmail.com>
2022-10-14 19:06:31 +0530

    Added:Usecase 1 for day 22 problems

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,6 +3,8 @@
  */
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 /**
  * @author Iam_A
  *
@@ -13,7 +15,40 @@ public class AddressBookMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String state;
+		String zip;
+		String phoneNo;
+		String email;
+		
+		
+		System.out.println("Welcome to Address Book");
+		System.out.print("Enter first name : ");
+		firstName = input.nextLine();
+		System.out.print("Enter last name :  ");
+		lastName = input.nextLine();
+		System.out.print("Enter address :  ");
+		address = input.nextLine();
+		System.out.print("Enter state :  ");
+		state = input.nextLine();
+		System.out.print("Enter city :  ");
+		city = input.nextLine();
+		System.out.print("Enter zip :  ");
+		zip = input.nextLine();
+		System.out.print("Enter phoneNo :  ");
+		phoneNo = input.nextLine();
+		System.out.print("Enter email :  ");
+		email = input.nextLine();		
+		
+		AddressBook addressBookObj1 = new AddressBook(firstName, lastName, address, city, state, zip,
+				 phoneNo, email);
+		addressBookObj1.displaData();
+		
+		input.close();
 
 	}
 
